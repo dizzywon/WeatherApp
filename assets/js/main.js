17693f79afef0e7fc612671112ffae2c682cca2a
dizzywon <dizegbe@gmail.com>
2018-05-16 07:11:04 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $("#banner-msg").typed({
+            strings: ["WELCOME TO WEATHER APP"],
+            typeSpeed: 40
+        })
+    }, 0);
+});
